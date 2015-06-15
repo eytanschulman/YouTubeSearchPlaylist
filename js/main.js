c9070e4f6ec4eebac2cacb0c70d3f26ff8d74c2a
Eytan Schulman <eytanschulman@gmail.com>
2015-06-15 11:58:42 -0400

    Tried to do a bunch of JS things

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 function doSomething() {
   var searchBoxString = document.getElementById("searchBox").value;
 
-  var test = "https://gdata.youtube.com/feeds/api/videos?q=" + encodeURI("Heros Cover");
+  var test = "https://gdata.youtube.com/feeds/api/videos?q=" + encodeURI(searchBoxString);
 
   console.log(encodeURI(searchBoxString));
-  console.log(doRequest(encodeURI(searchBoxString)));
+  // console.log(doRequest("http://jsonip.com/"));
+ doRequest(test);
 }
 
-function doRequest(urlToRequest) {
-  $.get(
-    urlToRequest,
-    {paramOne : 1, paramX : 'abc'},
-    function(data) {
-       alert('page content: ' + data);
-    }
-);
+function doRequest(url) {
+  $http.get(url).
+  success(function(data, status, headers, config) {
+    // this callback will be called asynchronously
+    // when the response is available
+    console.log("data: " + data);
+    console.log("status: "+ status);
+    console.log("headers: "+ headers);
+    console.log("config: "+config);
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
 }
