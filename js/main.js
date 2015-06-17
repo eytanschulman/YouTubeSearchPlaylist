0bfd84d8760b94a4e0645082e8c3823514edf95b
Eytan Schulman <eytanschulman@gmail.com>
2015-06-17 00:56:42 -0400

    Fixed videos not continuing, fixed new query not replacing old

--- js/main.js
+++ js/main.js
@@ -13,26 +13,18 @@ var currentIndex = 0;
 
 function doRequest(url) {
   $.get(url, function(data, status){
-    // this callback will be called asynchronously
-    // when the response is available
-    // console.log("data: " + data);
-    // console.log("JSON: " + JSON.stringify(data));
     console.log("status: "+ status);
 
     var jsonData = JSON.parse(JSON.stringify(data));
-    console.log("jsonData: "+ jsonData);
-    var arr = jsonData[1];
-    console.log(arr);
+
+    videoIDArray = [];
 
     for (var i=0; i<jsonData.items.length;i++) {
-      var item = jsonData.items[i];
-      var idTag = item["id"];
-      var vidID = idTag["videoId"];
-
-      videoIDArray.push(vidID);
+      videoIDArray.push(jsonData.items[i].id.videoId);
     }
 
     console.log("videoIDArray: "+videoIDArray);
+
     loadViewWithIndex(0);
 
   });
@@ -48,6 +40,22 @@ function onYouTubeIframeAPIReady() {
 
 function loadViewWithIndex(index) {
   if (index < 1) {
+
+    if(document.getElementById("videoPlayer") != undefined) {
+
+      console.log("Video Player exists");
+
+      var iframes = document.getElementsByTagName('iframe');
+      for (var i = 0; i < iframes.length; i++) {
+        iframes[i].parentNode.removeChild(iframes[i]);
+      }
+
+      var container = document.getElementById("video-container");
+      var newDiv = document.createElement('div');
+      newDiv.id = "videoPlayer";
+      container.appendChild(newDiv);
+    }
+
   var player = new YT.Player('videoPlayer', {
               height: '390',
               width: '640',
@@ -64,6 +72,11 @@ function loadViewWithIndex(index) {
         iframes[i].parentNode.removeChild(iframes[i]);
       }
 
+    var container = document.getElementById("video-container");
+    var newDiv = document.createElement('div');
+    newDiv.id = "videoPlayer";
+    container.appendChild(newDiv);
+
     var player = new YT.Player('videoPlayer', {
                   height: '390',
                   width: '640',
@@ -76,12 +89,10 @@ function loadViewWithIndex(index) {
     }
 }
 
-// autoplay video
         function onPlayerReady(event) {
             event.target.playVideo();
         }
 
-        // when video ends
         function onPlayerStateChange(event) {
             if(event.data === 0) {
                 if (currentIndex < videoIDArray.length) {
