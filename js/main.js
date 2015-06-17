e95f1e9aeaa977888fadaaa3098944d922c38f19
Eytan Schulman <eytanschulman@gmail.com>
2015-06-16 21:44:17 -0400

    Things are working! Version 1 of the prototype.
    Video autoplays when you do a search, but the next one isn’t starting
    up yet.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,88 @@
-// function doSomething() {
-//   var searchBoxString = document.getElementById("searchBox").value;
-//
-//   var test = "https://gdata.youtube.com/feeds/api/videos?q=" + encodeURI(searchBoxString);
-//
-//   console.log(encodeURI(searchBoxString));
-//   // console.log(doRequest("http://jsonip.com/"));
-//  doRequest(test);
-// }
-//
-// function doRequest(url) {
-//   $http.get(url).
-//   success(function(data, status, headers, config) {
-//     // this callback will be called asynchronously
-//     // when the response is available
-//     console.log("data: " + data);
-//     console.log("status: "+ status);
-//     console.log("headers: "+ headers);
-//     console.log("config: "+config);
-//   }).
-//   error(function(data, status, headers, config) {
-//     // called asynchronously if an error occurs
-//     // or server returns response with an error status.
-//   });
-// }
-window.runAction = function runAction() {
-  var req = createRequest(); // defined above
-  // Create the callback:
-  req.onreadystatechange = function() {
-    if (req.readyState != 4) return; // Not there yet
-    if (req.status != 200) {
-      // Handle request failure here...
-      return;
-    }
-    // Request successful, read the response
-    var resp = req.responseText;
-    console.log("response: "+ resp);
-    // ... and use it as needed by your app.
-  }
+function doSomething() {
+  var searchBoxString = document.getElementById("searchBox").value;
 
-  req.open("GET", "https://gdata.youtube.com/feeds/api/videos?q= +"encodeURI("Shut up and Dance"), true);
-  req.send();
+  var test = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q= "+ searchBoxString + "&key=AIzaSyBRHqgKlw9i17BaQdcM91PCSw_J4x-6yoc";
 
-  req.open("GET", url, true);
-  req.setRequestHeader("Content-Type",
-                       "text/json");
-  req.send(form-encoded request body);
+  console.log(searchBoxString);
+  // console.log(doRequest("http://jsonip.com/"));
+ doRequest(test);
 }
+
+var videoIDArray = [];
+var currentIndex = 0;
+
+function doRequest(url) {
+  $.get(url, function(data, status){
+    // this callback will be called asynchronously
+    // when the response is available
+    // console.log("data: " + data);
+    // console.log("JSON: " + JSON.stringify(data));
+    console.log("status: "+ status);
+
+    var jsonData = JSON.parse(JSON.stringify(data));
+    console.log("jsonData: "+ jsonData);
+    var arr = jsonData[1];
+    console.log(arr);
+
+    for (var i=0; i<jsonData.items.length;i++) {
+      var item = jsonData.items[i];
+      var idTag = item["id"];
+      var vidID = idTag["videoId"];
+
+      videoIDArray.push(vidID);
+    }
+
+    console.log("videoIDArray: "+videoIDArray);
+    loadViewWithIndex(0);
+
+  });
+}
+
+function onYouTubeIframeAPIReady() {
+  if (videoIDArray[0]) {
+    loadViewWithIndex(currentIndex);
+  } else {
+    console.log("There's no data in videoIDArray yet.");
+  }
+}
+
+function loadViewWithIndex(index) {
+  var player;
+  if (index < 1) {
+  player = new YT.Player('videoPlayer', {
+              height: '390',
+              width: '640',
+              videoId: ""+videoIDArray[index],
+              events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+              }
+            });
+    } else {
+      player = new YT.Player('videoPlayer', {
+                  height: '390',
+                  width: '640',
+                  videoId: ""+videoIDArray[index],
+                  events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                  }
+                });
+        document.getElementById("videoPlayer").innerHTML = player;
+    }
+}
+
+// autoplay video
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+
+        // when video ends
+        function onPlayerStateChange(event) {
+            if(event.data === 0) {
+                if (currentIndex < videoIDArray.length) {
+                  currentIndex++;
+                  loadViewWithIndex(currentIndex);
+                }
+            }
+        }
