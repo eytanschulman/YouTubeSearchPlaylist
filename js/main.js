e9909ea4c35ab243749daa0cc991472fbcded13a
Austin Jackson <au5ton@icloud.com>
2015-06-17 15:16:56 -0500

    Lots of new Materialize style
    :sparkles: SUPER STYLISH :sparkles:

--- js/main.js
+++ js/main.js
@@ -1,101 +1,105 @@
-function doSomething() {
-  var searchBoxString = document.getElementById("searchBox").value;
-
-  var test = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q= "+ searchBoxString + "&key=AIzaSyBRHqgKlw9i17BaQdcM91PCSw_J4x-6yoc";
-
-  console.log(searchBoxString);
-  // console.log(doRequest("http://jsonip.com/"));
- doRequest(test);
-}
-
 var videoIDArray = [];
 var currentIndex = 0;
 
+
+
+
+
+function doSomething() {
+    var searchBoxString = document.getElementById("searchBox").value;
+
+    var test = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q= "+ searchBoxString + "&key=AIzaSyBRHqgKlw9i17BaQdcM91PCSw_J4x-6yoc";
+
+    console.log(searchBoxString);
+    // console.log(doRequest("http://jsonip.com/"));
+    doRequest(test);
+}
+
+
+
 function doRequest(url) {
-  $.get(url, function(data, status){
-    console.log("status: "+ status);
+    $.get(url, function(data, status){
+        console.log("status: "+ status);
 
-    var jsonData = JSON.parse(JSON.stringify(data));
+        var jsonData = JSON.parse(JSON.stringify(data));
 
-    videoIDArray = [];
+        videoIDArray = [];
 
-    for (var i=0; i<jsonData.items.length;i++) {
-      videoIDArray.push(jsonData.items[i].id.videoId);
-    }
+        for (var i=0; i<jsonData.items.length;i++) {
+            videoIDArray.push(jsonData.items[i].id.videoId);
+        }
 
-    console.log("videoIDArray: "+videoIDArray);
+        console.log("videoIDArray: "+videoIDArray);
 
-    loadViewWithIndex(0);
+        loadViewWithIndex(0);
 
-  });
+    });
 }
 
 function onYouTubeIframeAPIReady() {
-  if (videoIDArray[0]) {
-    loadViewWithIndex(currentIndex);
-  } else {
-    console.log("There's no data in videoIDArray yet.");
-  }
+    if (videoIDArray[0]) {
+        loadViewWithIndex(currentIndex);
+    } else {
+        console.log("There's no data in videoIDArray yet.");
+    }
 }
 
 function reset() {
-  var iframes = document.getElementsByTagName('iframe');
-  for (var i = 0; i < iframes.length; i++) {
-    iframes[i].parentNode.removeChild(iframes[i]);
-  }
-  var searchBox = document.getElementById("searchBox");
-  searchBox.value = "";
+    var iframes = document.getElementsByTagName('iframe');
+    for (var i = 0; i < iframes.length; i++) {
+        iframes[i].parentNode.removeChild(iframes[i]);
+    }
+    var searchBox = document.getElementById("searchBox");
+    searchBox.value = "";
 }
 
 
 function loadViewWithIndex(index) {
 
-      var iframes = document.getElementsByTagName('iframe');
-      for (var i = 0; i < iframes.length; i++) {
+    var iframes = document.getElementsByTagName('iframe');
+    for (var i = 0; i < iframes.length; i++) {
         iframes[i].parentNode.removeChild(iframes[i]);
-      }
-
-      var container = document.getElementById("video-container");
-      var newDiv = document.createElement('div');
-      newDiv.id = "videoPlayer";
-      container.appendChild(newDiv);
-
-      var player = new YT.Player('videoPlayer', {
-                height: '390',
-                width: '640',
-                videoId: ""+videoIDArray[index],
-                events: {
-                  'onReady': onPlayerReady,
-                  'onStateChange': onPlayerStateChange
-                }
-              });
     }
 
-        function onPlayerReady(event) {
-            event.target.playVideo();
-        }
+    var container = document.getElementById("video-container");
+    var newDiv = document.createElement('div');
+    newDiv.id = "videoPlayer";
+    container.appendChild(newDiv);
 
-        function onPlayerStateChange(event) {
-            if(event.data === 0) {
-                if (currentIndex < videoIDArray.length) {
-                  currentIndex++;
-                  loadViewWithIndex(currentIndex);
-                }
-            }
+    var player = new YT.Player('videoPlayer', {
+        videoId: ""+videoIDArray[index],
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
         }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    if(event.data === 0) {
+        if (currentIndex < videoIDArray.length) {
+            currentIndex++;
+            loadViewWithIndex(currentIndex);
+        }
+    }
+}
 
 $(document).keydown(function(event){
     var key = event.which;
-            switch(key) {
-              case 37:
-                  // Key left.
-                  currentIndex -= 1;
-                  loadViewWithIndex(currentIndex);
-                  break;
-              case 39:
-                  // Key right.
-                  currentIndex += 1;
-                  loadViewWithIndex(currentIndex);
-                  break;
-        }
-  });
+    switch(key) {
+        case 37:
+        // Key left.
+        currentIndex -= 1;
+        loadViewWithIndex(currentIndex);
+        break;
+        case 39:
+        // Key right.
+        currentIndex += 1;
+        loadViewWithIndex(currentIndex);
+        break;
+    }
+});
