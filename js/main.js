dd980fd0940e871439fd442cbae331c3cda6e592
Austin Jackson <au5ton@icloud.com>
2015-06-18 15:57:20 -0500

    Added new Playlist View

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-var videoIDArray = [];
+
+var playlist = [];
 var currentIndex = 0;
 var maxResults = "50";
 var searchResults;
 
 var apiKey = "AIzaSyC5lG6cr07lMFM_NjAiL3M8kd0Kgmz92-I";
+
+//Important URLs:
+//https://www.googleapis.com/youtube/v3/videos?part=snippet&id=SK3NbwUrzwY&key=AIzaSyC5lG6cr07lMFM_NjAiL3M8kd0Kgmz92-I
+//https://www.googleapis.com/youtube/v3/channels?part=snippet&id=UCmWxBj64pEUXnM_AVgaRaMQ&key=AIzaSyC5lG6cr07lMFM_NjAiL3M8kd0Kgmz92-I
 
 function reset() {
     $("#video-container").empty();
@@ -37,14 +42,35 @@ function searchSuccessful() {
     }
     else {
 
-        videoIDArray = [];
+        playlist = [];
+        console.log("searchResults");
+        console.log(searchResults);
 
         for (var i=0; i<searchResults.items.length;i++) {
             if(searchResults.items[i].id.kind === "youtube#video") {
-                videoIDArray.push(searchResults.items[i].id.videoId);
+                //videoIDArray.push(searchResults.items[i].id.videoId);
+                playlist.push({
+                    title: searchResults.items[i].snippet.title,
+                    authorTitle: searchResults.items[i].snippet.channelTitle,
+                    authorId: searchResults.items[i].snippet.channelId,
+                    videoId: searchResults.items[i].id.videoId,
+                    thumbnail: searchResults.items[i].snippet.thumbnails.default.url
+                });
             }
         }
-        console.log(videoIDArray)
+        for(var i = 0; i < playlist.length; i++) {
+            var elem = createPlaylistItemElement({
+                playlistNumber: i+1,
+                title: playlist[i].title,
+                videoId: playlist[i].videoId,
+                author: playlist[i].authorTitle,
+                authorId: playlist[i].authorId,
+                thumbnail: playlist[i].thumbnail
+            });
+            $(elem.children[0]).removeClass("not-shown");
+            $("#playlist-preview").append(elem);
+        }
+        console.log(playlist);
         if(currentIndex === 0) {
             loadVideoWithIndex(0);
         }
@@ -62,7 +88,7 @@ function loadVideoWithIndex(index) {
     container.append(tempDiv);
 
     var player = new YT.Player('videoPlayer', {
-        videoId: ""+videoIDArray[index],
+        videoId: String(playlist[index].videoId),
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange,
@@ -73,23 +99,38 @@ function loadVideoWithIndex(index) {
 
 function onPlayerReady(event) {
     event.target.playVideo();
-    $("#pld-progress").html("Video "+(currentIndex+1)+"/"+videoIDArray.length);
+    $("#pld-progress").html("Video "+(currentIndex+1)+"/"+playlist.length);
 }
 
 function onPlayerStateChange(event) {
-    console.log(event);
+    //console.log(event);
     if(event.data === 1) {
         //If the video starts playing
         $("#pld-loading").addClass("not-shown");
     }
     if(event.data === 0) {
-        if (currentIndex < videoIDArray.length) {
+        if (currentIndex < playlist.length) {
             currentIndex++;
             loadVideoWithIndex(currentIndex);
-        } else if(currentIndex === videoIDArray.length-1){
+        } else if(currentIndex === playlist.length-1){
             //If it's the last video in the result list...
-
+            //TODO
             //Reload some more results and append them to the videoIDArray
         }
     }
+}
+
+function createPlaylistItemElement(t) {
+
+    var elem = document.createElement("div");
+    elem.innerHTML = document.getElementById("playlist-preview-item-template").innerHTML;
+    console.log(elem);
+    elem.innerHTML = String(elem.innerHTML).replace("{{PLAYLIST_NUMBER}}",t.playlistNumber);
+    elem.innerHTML = String(elem.innerHTML).replace("{{PLAYLIST_VIDEO_TITLE}}",t.title);
+    elem.innerHTML = String(elem.innerHTML).replace("{{PLAYLIST_VIDEO_ID}}",t.videoId);
+    elem.innerHTML = String(elem.innerHTML).replace("{{PLAYLIST_VIDEO_AUTHOR}}",t.author);
+    elem.innerHTML = String(elem.innerHTML).replace("{{PLAYLIST_VIDEO_AUTHOR_ID}}",t.authorId);
+    elem.innerHTML = String(elem.innerHTML).replace("{{PLAYLIST_VIDEO_THUMBNAIL}}",t.thumbnail);
+    console.log(elem);
+    return elem;
 }
